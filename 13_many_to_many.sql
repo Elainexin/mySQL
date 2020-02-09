988f70b6913e8d1265056bb7ad27519cd1a8e449
Yanlian Xin <xinyanlian@outlook.com>
2020-02-09 16:39:47 -0500

    WHERE rating IS NULL

--- 13_many_to_many.sql
+++ 13_many_to_many.sql
@@ -4,8 +4,29 @@
 
 
 -- challenge 2 
-SELECT title, AVG(rating) AS avg_rating FROM series
-JOIN reviews 
-	ON series.id = reviews.series_id
-	GROUP BY series.id
-	ORDER BY avg_rating;
+-- SELECT title, AVG(rating) AS avg_rating FROM series
+-- JOIN reviews 
+-- 	ON series.id = reviews.series_id
+-- 	GROUP BY series.id
+-- 	ORDER BY avg_rating;
+
+-- challenge 3 
+-- SELECT first_name, last_name, rating FROM reviewers
+-- JOIN reviews
+-- 	ON reviewers.id = reviews.reviewer_id;
+
+--  challenge 4
+-- SELECT title AS unreviewed_series FROM series
+-- LEFT JOIN reviews
+-- 	ON series.id = reviews.series_id
+-- WHERE rating IS NULL; 
+-- can't use rating = NULL 
+
+-- challenge 5
+SELECT genre, ROUND(AVG(rating),2) AS avg_rating FROM series
+JOIN reviews
+ ON series.id = reviews.series_id
+GROUP BY genre;
+
+
+
